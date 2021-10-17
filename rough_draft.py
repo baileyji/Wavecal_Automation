1d28ebe2d9769ef5803eac620a8b97be28683508
mailesasaki <69178214+mailesasaki@users.noreply.github.com>
2021-10-17 14:20:15 -0700

    Add files via upload

---
+++ rough_draft.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 22:18:35 2021
+
+@author: Maile Sasaki
+
+This code is for the ease of communication with the LLTF Contrast at the Subaru Observatory.
+"""
+
+from ctypes import *
+
+class NKTContrast():
+    """
+    This class performs various functions in the NKT Photon instrument.
+    
+    """
+    def status():
+        """
+        Returns status of the instrument.
+        -------
+        None.
+
+        """
+        
+    def wavecal(wave1, wave2):
+        """
+        
+
+        Parameters
+        ----------
+        wave1 : TYPE
+            DESCRIPTION.
+        wave2 : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        
+if __name__ == '__main__':
+    
